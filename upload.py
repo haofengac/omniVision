80da104442cd42de3ce4afd05b4b529e11809d3f
bhpfelix <bhpfelix@gmail.com>
2016-09-24 10:37:40 -0400

    Added dropbox upload functionality

--- upload.py
+++ upload.py
@@ -1,14 +1,32 @@
-from firebase import firebase as fb
-auth = fb.FirebaseAuthentication('hLayIOoCcxdSMSVoN5TaN8yLX97lfRlDqsFsptLo', 'bhpfelix@gmail.com')
-# , auth_payload={'uid': 'rgYuTjBxZ6cTjUUM35YVxcO6CYb2'}
-# NB renamed extras -> auth_payload, id -> uid here
-firebase = fb.FirebaseApplication('https://omnivision-a2b18.firebaseio.com', authentication=auth)
-result = firebase.get('/messages', name=None, connection=None,
-                      params={'print': 'pretty'})
-# HTTPError: 401 Client Error: Unauthorized
-print result
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import dropbox
 
-new_user = 'Ozgur Vatansever'
+class TransferData:
+    def __init__(self, access_token):
+        self.access_token = access_token
+        self.dbx = dropbox.Dropbox(self.access_token)
+        self.client = dropbox.client.DropboxClient(self.access_token)
 
-result = firebase.post('/users', new_user)
-print result
+    def upload_file(self, file_from, file_to):
+        """upload a file to Dropbox using API v2
+        """
+
+        with open(file_from, 'rb') as f:
+            self.dbx.files_upload(f, file_to)
+
+        print self.client.share('/omniVision.png', short_url=False)
+
+def main():
+    access_token = 'rDTXhwIRvIAAAAAAAAAACA04mgp3sK5gF05qTBk3KtbgMyScHMgfeLen1b_BLkde'
+    transferData = TransferData(access_token)
+
+    file_from = 'omniVision.png'
+    file_to = '/omniVision.png'  # The full path to upload the file to, including the file name
+
+    # API v2
+    transferData.upload_file(file_from, file_to)
+
+
+if __name__ == '__main__':
+    main()
