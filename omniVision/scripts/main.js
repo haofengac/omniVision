350439caa36e2773420942f692fa8acb2ffbf506
Wenqi Xian <wxian3@gatech.edu>
2016-09-25 02:00:56 -0400

    Documentation, icon added, and styling.

--- omniVision/scripts/main.js
+++ omniVision/scripts/main.js
@@ -107,67 +107,6 @@ OmniVision.prototype.saveMessage = function(e) {
 };
 */
 
-// Sets the URL of the given img element with the URL of the image stored in Firebase Storage.
-OmniVision.prototype.setImageUrl = function(imageUri, imgElement) {
-  /* If the image is a Firebase Storage URI we fetch the URL.
-  if (imageUri.startsWith('gs://')) {
-    imgElement.src = OmniVision.LOADING_IMAGE_URL; // Display a loading image first.
-    this.storage.refFromURL(imageUri).getMetadata().then(function(metadata) {
-      imgElement.src = metadata.downloadURLs[0];
-    });
-  } else {*/
-    imgElement.src = imageUri;
-//  }
-};
-
-
-// Saves a new message containing an image URI in Firebase.
-/* This first saves the image in Firebase storage.
-OmniVision.prototype.saveImageMessage = function(event) {
-
-  var file = event.target.files[0];
-
-  // Clear the selection in the file picker input.
-  this.imageForm.reset();
-
-  // Check if the file is an image.
-  if (!file.type.match('image.*')) {
-    var data = {
-      message: 'You can only share images',
-      timeout: 2000
-    };
-    this.signInSnackbar.MaterialSnackbar.showSnackbar(data);
-    return;
-  }
-
-  // Check if the user is signed-in
-  if (this.checkSignedInWithMessage()) {
-
-    // We add a message with a loading icon that will get updated with the shared image.
-    var currentUser = this.auth.currentUser;
-    this.messagesRef.push({
-      name: currentUser.displayName,
-      imageUrl: OmniVision.LOADING_IMAGE_URL,
-      photoUrl: currentUser.photoURL || '/images/profile_placeholder.png'
-    }).then(function(data) {
-
-      // Upload the image to Firebase Storage.
-      var uploadTask = this.storage.ref(currentUser.uid + '/' + Date.now() + '/' + file.name)
-          .put(file, {'contentType': file.type});
-      // Listen for upload completion.
-      uploadTask.on('state_changed', null, function(error) {
-        console.error('There was an error uploading a file to Firebase Storage:', error);
-      }, function() {
-
-        // Get the file's Storage URI and update the chat message placeholder.
-        var filePath = uploadTask.snapshot.metadata.fullPath;
-        data.update({imageUrl: this.storage.ref(filePath).toString()});
-      }.bind(this));
-    }.bind(this));
-  }
-};
-*/
-
 // Signs-in Omni Vision.
 OmniVision.prototype.signIn = function() {
   // Sign in Firebase using popup auth and Google as the identity provider.
@@ -271,7 +210,7 @@ OmniVision.prototype.displayMessage = function(key, name, text, picUrl, imageUri
     image.addEventListener('load', function() {
       this.messageList.scrollTop = this.messageList.scrollHeight;
     }.bind(this));
-    this.setImageUrl(imageUri, image);
+    image.src = imageUri;
     messageElement.innerHTML = '';
     messageElement.appendChild(image);
   }
@@ -326,6 +265,7 @@ window.onload = function() {
   window.omniVision = new OmniVision();
 };
 
+// Desktop push notification setting
 function notificationSetting() {
   // Check if the browser supports notifications
   if (!("Notification" in window)) {
